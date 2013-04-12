879aaa60df85f6d94c2e4610dc0c15e8cb52ecf0
Daleth7 <i.publish.docs@gmail.com>
2013-04-12 14:10:52 -0700

    Update Operations.h

--- Operations.h
+++ Operations.h
@@ -10,6 +10,8 @@ namespace operations
    double divide(double, double); // Returns sum.
    int factorial_int(double);
    double factorial_dec(double); //Uses gamma function for decimal factorials; complex number implementation not updated
+   double Logarithm_Natural(double);
+   double Logarithm_Base10(double);
 }
 
 
