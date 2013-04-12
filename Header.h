10e12ce71fd45f433b91062388bd57ce4660ef18
Olysold <rotmgbiz@gmail.com>
2013-04-13 02:55:28 +0800

    Create Header.h

---
+++ Header.h
@@ -0,0 +1,12 @@
+double divide()
+    {
+        double firstInput, secondInput;
+
+        std::cin >> firstInput >> secondInput;
+
+        firstInput /= secondInput;
+
+        std::cout << firstInput << std::endl;
+
+        return firstInput;
+    }
