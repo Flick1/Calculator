91305a68602f15a6084e9df312aef3e2b88985d3
Olysold <rotmgbiz@gmail.com>
2013-04-13 02:59:03 +0800

    Create Operations.cpp

---
+++ Operations.cpp
@@ -0,0 +1,14 @@
+
+
+double division()
+{
+        double firstInput, secondInput;
+
+        std::cin >> firstInput >> secondInput;
+
+        firstInput /= secondInput;
+
+        std::cout << firstInput << std::endl;
+
+        return firstInput;
+}
