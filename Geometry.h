c17b8d0db770b9d076a24ced2debae80e6056d90
Olysold <rotmgbiz@gmail.com>
2013-04-13 05:02:17 +0800

    Update Geometry.h

--- Geometry.h
+++ Geometry.h
@@ -2,16 +2,17 @@
 #define GEOMETRY_H
 
   namespace Geometry{
-    Area_R(double,double);  //Calculate rectangular area given height and width
-    Area_C(double);   //Calculate circular area given radius
-    Area_Tri(double,double);  //Calculate triangular area given height and base width
-    Area_ETri(double); //Calculate equilateral triangular area given height or base width
-    Area_Trap(double,double,double); //Calculate trapizoidal area given height and widths of bases
+      Area_R(double,double);  //Calculate rectangular area given height and width
+      Area_C(double);   //Calculate circular area given radius
+      Area_Tri(double,double);  //Calculate triangular area given height and base width
+      Area_ETri(double); //Calculate equilateral triangular area given height or base width
+      Area_Trap(double,double,double); //Calculate trapizoidal area given height and widths of bases
   }
   
-  namespace Volume{
-    double Vol_Cube(double, double, double);
-    double Vol_RectC(double, double, double);
+  namespace volume{
+      double vol_cube(double, double, double);
+      double vol_rectC(double, double, double);
+      double vol_cylin(double, double);
   }
   
 #endif
