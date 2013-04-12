796ff624cb111e4e2a3dcfe5224f8f9cba89165c
Daleth7 <i.publish.docs@gmail.com>
2013-04-12 12:56:25 -0700

    Update Operations.h

--- Operations.h
+++ Operations.h
@@ -7,9 +7,8 @@ namespace operations
    double add();
    double subtract(double,double); //return sum
    double multiply();  //Prompt user to enter in an undefined number of factors and return their product
-   double 
    double divide(); //Prints text to acknowledge the current operation and number to input (first/second). Returns sum.
-   
+   int factorial_int(double);
 }
 
 
