15cb18634ac52f0f8326e29639ce1871d16a0bfe
Olysold <rotmgbiz@gmail.com>
2013-04-13 04:59:14 +0800

    Update Volume.cpp

--- Volume.cpp
+++ Volume.cpp
@@ -1,19 +1,38 @@
 #include "Geometry.h"
 
-double Volume::Vol_Cube(double height, double width, double length)
+double Volume::Vol_Cube(double cubeHeight, double cubeWidth, double cubeLength)
 {
-    double sum;
+    double cubeSum;
 
     try
     {
         if (height == width && height == length) //Test if this is indeed a cube.
         {
-            sum = height * width * length;
-            return sum;
+            cubeSum = height * width * length;
+            return cubeSum;
         }
         else
             throw 99;
     } catch(int err) { }
 
     return 1;
+}
+
+double Volume::Vol_RectC(double rectHeight, double rectWidth, double rectLength)
+{
+    double rectSum;
+    
+    try
+    {
+        if (height != width || height != length) //Test if this is indeed a cuboid.
+        {
+            rectSum = height * width * length;
+            return cubeSum;
+        }
+        else
+            throw 98;
+    } catch(int err) { }
+    
+    return 1;
+     
 }
