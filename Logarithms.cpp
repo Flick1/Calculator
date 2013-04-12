58e13b7cbcd9e68c8dbef4bdd6d64dda5ec1077e
Daleth7 <i.publish.docs@gmail.com>
2013-04-12 14:08:00 -0700

    Create Logarithms.cpp

---
+++ Logarithms.cpp
@@ -0,0 +1,5 @@
+#include "Operations.h"
+#include <cmath>
+
+double Operations::Logarithm_Natural(double number){return log(number);}
+double Operations::Logarithm_Base10(double number){return log10(number);}
