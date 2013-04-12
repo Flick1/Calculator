cc956cc64173ffee001c1be281bd5c479a83b4a5
Daleth7 <i.publish.docs@gmail.com>
2013-04-12 12:09:38 -0700

    Update Operations.h

--- Operations.h
+++ Operations.h
@@ -4,6 +4,8 @@
 
 namespace operations
 {
+   double add();
+   double subtract(); //prompt user to enter in numbers and return sum
    double multiply();  //Prompt user to enter in an undefined number of factors and return their product
    double divide();
    double 
