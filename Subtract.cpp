82ba350848b969c2df27bb3b82628a231fafc825
Daleth7 <i.publish.docs@gmail.com>
2013-04-12 12:58:31 -0700

    Update Subtract.cpp

--- Subtract.cpp
+++ Subtract.cpp
@@ -1,3 +1,3 @@
 #include "Operations.h"
 
-double operations::subtract(double first,double second){return (firt - second);}
+double operations::subtract(double first,double second){return (first - second);}
