bfbf6e8c01c5878e66dfeb5a2da5ef2476884e97
Dreamer22 <mdbr2@hotmail.com>
2013-04-13 09:44:38 +0300

    Update and rename Pow & Square to Pow & Square.cpp

---
+++ Calculator/Pow & Square.cpp
@@ -0,0 +1,10 @@
+#include <cmath>
+
+double operations::Power(double Number, double Power)
+{
+     return(pow(Number ,Power));  
+}                      
+double operations::Square(double Number)
+{
+    return (sqrt(Number));
+}
