685496881a94f3263666d48a3c55b122bd3ebf5e
Olysold <rotmgbiz@gmail.com>
2013-04-14 03:01:19 +0800

    Update Geometry.h

--- Geometry.h
+++ Geometry.h
@@ -1,21 +1,25 @@
 #ifndef GEOMETRY_H
 #define GEOMETRY_H
 
- /* namespace{
-    const double PI__ 3.14159;  //Place pi in global namespace due to use in multiple modules
-  }*/
-  namespace Geometry{
-      double Area_R(double,double);  //Calculate rectangular area given height and width
-      double Area_C(double);   //Calculate circular area given radius
-      double Area_Tri(double,double);  //Calculate triangular area given height and base width
-      double Area_ETri(double); //Calculate equilateral triangular area given height or base width
-      double Area_Trap(double,double,double); //Calculate trapizoidal area given height and widths of bases
+  namespace {
+    const double PI__ = 3.14159;  //Place pi in global namespace due to use in multiple modules
   }
 
-  namespace volume{
-      double vol_cube(double);
-      double vol_rectC(double, double, double);
-      double vol_cylin(double, double);
+  namespace Area{
+      double Area_R(double, double);  //Calculate rectangular area given height and width
+      double Area_C(double);   //Calculate circular area given radius
+      double Area_Tri(double, double);  //Calculate triangular area given height and base width
+      double Area_ETri(double); //Calculate equilateral triangular area given side
+      double Area_Trap(double, double, double); //Calculate trapizoidal area given height and widths of bases
+  }
+
+  namespace Volume{
+      double Vol_Cube(double); //Calculate volume of cube. Passed one value representing width, length and height.
+      double Vol_RectC(double, double, double); //Calculate volume of cuboid given height, width and length. One variable must be different in value.
+      double Vol_Cylin(double, double); //Calculate volume of cylinder given radius and height.
+      double Vol_Pyra(double); //Calculate volume of pyramid given height. Calculation for base (Area of square) is requested within the definition.
+      double Vol_Cone(double); //Calculate volume of cone given height. Calculation for base (Area of circle) is requested within the definition.
+      double Vol_Sphe(double); //Calculate volume of sphere given radius.
   }
 
   namespace Surface_Area{
