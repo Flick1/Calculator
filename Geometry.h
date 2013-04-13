9857ef76a8ef3d1290979055d21c25e5a804e7f3
Daleth7 <i.publish.docs@gmail.com>
2013-04-12 19:02:03 -0700

    Update Geometry.h

--- Geometry.h
+++ Geometry.h
@@ -15,4 +15,10 @@
       double vol_cylin(double, double);
   }
   
+  namespace Surface_Area{
+    SArea_S(double);  //Calculate surface area of a sphere given radius
+    SArea_R(double, double, double); //Calculate surface area of a rectangular prism given length, width, and height
+    SArea_C(double);   //Calculate the surface area of a cube given length, width, or height
+  }
+  
 #endif
