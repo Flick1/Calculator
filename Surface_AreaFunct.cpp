632992c2cbc3b6f1fb29f9834b46f18aa96f1bc7
Daleth7 <i.publish.docs@gmail.com>
2013-04-12 19:02:59 -0700

    Create Surface_AreaFunct.cpp

---
+++ Surface_AreaFunct.cpp
@@ -0,0 +1,5 @@
+#include "Operation.h"
+
+SArea_S(double); //Calculate surface area of a sphere given radius
+    SArea_R(double, double, double); //Calculate surface area of a rectangular prism given length, width, and height
+    SArea_C(double); //Calculate the surface area of a cube given length, width, or height
