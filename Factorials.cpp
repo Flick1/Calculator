c3e5be469b184d45da3630e5cc6d7552ea0357b5
Daleth7 <i.publish.docs@gmail.com>
2013-04-12 20:36:15 -0700

    Update Factorials.cpp

--- Factorials.cpp
+++ Factorials.cpp
@@ -14,7 +14,8 @@ int operations::factorial_int(double input){
 double operations::factorial_dec(double input){
   int limit = 0;
   double summation = 0;
-  while(limit != ~0){  //Gamma function, sum e^-limit * limit^(input-1) from 0 to maximum integer value minus 1
+  while(limit != 32769){  //Gamma function, sum e^-limit * limit^(input-1) from 0 to arbitrary maximum
+                          //INT_MAX and ~0 avoided because of runtime cost
      summation += exp(limit*(-1))*pow(limit, input-1);
      limit++;
   }
