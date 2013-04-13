1242fd42892426708003d88f09a05379cf7394f6
Daleth7 <i.publish.docs@gmail.com>
2013-04-12 19:40:17 -0700

    Update Factorials.cpp
    Fixed member names.

--- Factorials.cpp
+++ Factorials.cpp
@@ -2,17 +2,17 @@
 #include <cmath>
 #include <limits.h>
 
-int Geometry::factorial_int(double input){
+int operations::factorial_int(double input){
   int whole = input;
     if((input - whole) != 0.0)   //Test if parameter is an integer
        throw "Error. Parameter not an integer. Use factorial_dec instead.";
     else if(input == 0)   //Stop recursion to avoid negative values
       return 1;
     else
-      return input*factorial_int(input-1);
+      return input*operations::factorial_int(input-1);
 }
 
-double Geometry::factorial_dec(double input){
+double operations::factorial_dec(double input){
   int limit = 0;
   double summation = 0;
   while(limit != INT_MAX){  //Gamma function, sum e^-limit * limit^(input-1) from 0 to maximum integer value
