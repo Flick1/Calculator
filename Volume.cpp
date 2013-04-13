915cb6c060cfa3fc7e3d28071f35b838f523b0dd
Olysold <rotmgbiz@gmail.com>
2013-04-13 23:56:44 +0800

    Update Volume.cpp

--- Volume.cpp
+++ Volume.cpp
@@ -27,9 +27,8 @@ double volume::vol_rectC(double rectHeight, double rectWidth, double rectLength)
 double volume::vol_cylin(double radius, double height)
 {
     double cylinSum;
-    const double pi = 3.14;
     
-    cylinSum = pi * pow(radius, 2) * height;
+    cylinSum = PI__ * pow(radius, 2) * height;
     return cylinSum;   
 }
     
