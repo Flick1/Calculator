26cfaf56842456ae60f180f8833b8661d128baab
Daleth7 <i.publish.docs@gmail.com>
2013-04-12 19:42:49 -0700

    Update Surface_AreaFunct.cpp

--- Surface_AreaFunct.cpp
+++ Surface_AreaFunct.cpp
@@ -13,6 +13,6 @@ double Surface_Area::SArea_S(double radius){return 4*PI__*radius*radius;}
     -------------------
         ^ length
   */
-double Surface_Area::SArea_R(double length, double width, double height){return (2*length*width + 4*height*width);}
+double Surface_Area::SArea_R(double length, double width, double height){return (2*length*width + 4*height*length);}
 double Surface_Area::SArea_C(double edgelength){return 6*edgelength*edgelength;}
 double Surface_Area::SArea_Cyl(double radius,double height){return (2*PI__*radius*radius + 2*PI__*radius*height);}
