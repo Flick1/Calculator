f530926dd9880c3ae7f263f90b6d186f7acaeaf4
Olysold <rotmgbiz@gmail.com>
2013-04-17 20:52:46 +0800

    Lack of error handling, but functional with user cooperation.

---
+++ Storage.h
@@ -0,0 +1,22 @@
+#ifndef STORING_H_INCLUDED
+#define STORING_H_INCLUDED
+
+#include<iostream>
+#include<vector>
+#include<string>
+
+
+class Storage
+{
+private:
+    static std::vector<std::vector<double>> data;
+    std::vector<std::string> names {"Subtraction", "Addition", "Divide", "Multiply"};
+    
+public:
+    static void store(const int, const double);
+    void printMenu();
+    double getData(std::size_t, double);
+};
+
+
+#endif // STORING_H_INCLUDED
