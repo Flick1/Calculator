6abdf1391d9670328e69cd1c8d08ed24358fa699
Olysold <rotmgbiz@gmail.com>
2013-04-17 21:01:25 +0800

    Edited to demonstrate storage.

--- Subtract.cpp
+++ Subtract.cpp
@@ -1,3 +1,11 @@
 #include "Operations.h"
+#include "Storing.h"
 
-double operations::subtract(double first,double second){return (first - second);}
+double operations::subtract(double first,double second)
+{
+    double sum = first - second;
+
+    Storage::store(0, sum);
+
+    return sum;
+}
