d2005be8db12baebe101f06216dcc4e5d0613161
Olysold <rotmgbiz@gmail.com>
2013-04-18 15:53:11 +0800

    Changed header name and corrected error.

--- TestStorageClass.cpp
+++ TestStorageClass.cpp
@@ -1,4 +1,4 @@
-//Requires the following header includes, Subtract.cpp, Storing.cpp, and Input.cpp.
+//Requires the following header includes, Subtract.cpp, Storage.cpp, and Input.cpp.
 //Lacks proper error handling. Been trying to figure out a way to deal with it (ie. user selecting containers that
 //don't exist) but I can't seem to find a decent source that teaches about error handling.
 //To get data from stored menu, selection starts at one. Then input the exact number you want to get.
@@ -10,7 +10,7 @@
 #include <iostream>
 #include "Handler.h"
 #include "Operations.h"
-#include "Storing.h"
+#include "Storage.h"
 
 using std::vector;
 using std::string;
@@ -18,7 +18,7 @@ using std::string;
 int main()
 {
     double input[4] = {0,0,0,0};
-    double select[1];	//See comment on line 32
+    double select[2];	//See comment on line 32
     Storage test;
 
     std::cout << "You're going to subtract: " << std::endl;
@@ -31,6 +31,7 @@ int main()
     test.printMenu();
 
     input::get(2, select);	//select is declared as an array of size 1...
+    				//somehow it still worked in codeblocks =/, changed it.
     std::cout << test.getData(select[0], select[1]) << std::endl;
 
     std::cout << "Subtracting with selected data." << std::endl;
