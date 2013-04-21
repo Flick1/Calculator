9e517b9dea754af908bc933e8190fd04a9d771d2
Olysold <rotmgbiz@gmail.com>
2013-04-21 22:01:29 +0800

    Support enum select. Makeshift error handling.

--- Storage.cpp
+++ Storage.cpp
@@ -15,57 +15,81 @@ void Storage::store(const std::size_t selectContain, const double inNum){
     data.at(selectContain).push_back(inNum);
 }
 
+
+MENUSELECT Storage::selectMenu(std::string &s) //Convert string input to integer used in menu selection.
+{
+    MENUSELECT sm = DEFAULT;
+
+    if (s == "Subtraction" || s == "subtraction") { sm = Subtraction; }
+    if (s == "Addition" || s == "addition") { sm = Addition; }
+    if (s == "Divide" || s == "divide") { sm = Divide; }
+    if (s == "Multiply" || s == "multiply") { sm = Multiply; }
+
+    return sm;
+}
+
 //Displays stored data along with the appropriate name.
 void Storage::printMenu()
 {
-    std::size_t namePos = 1; //To seek out the appropriate name of the operation from a vector of const names.
-    std::size_t cnt = 1; //Selection numbering to aid user in selecting available containers.
+    std::size_t namePos = 1;
+    std::size_t cnt = 0;
 
     for (const auto outVec : data)
     {
-        if(!outVec.empty()) //Searches vector<vector<double>> data and only processes if it isn't empty.
+        if(!outVec.empty())
         {
-            std::cout << cnt << ". " << names[namePos - 1] << ": "; //Operation name and location in data.
+            std::cout << names[namePos - 1] << ": ";
 
             for (auto inVec : outVec)
             {
-                std::cout << inVec << ", "; //Print out stored data within vector element.
+                std::cout << "(" << cnt << ")" << inVec << " ";
+                ++cnt;
             }
-            std::cout << std::endl;           
+            std::cout << std::endl;
         }
-        ++cnt;
         ++namePos;
     }
 }
 
 //Request the selected container (row) and the specific number.
-double Storage::getData(const std::size_t row, const double item)
+double Storage::getData(MENUSELECT &row, double &item) //item should be type size_t now (I guess), but input function
+						       //can only support double types.
 {
-    std::vector<double>::iterator srch;
-
-    srch = std::find(data[row - 1].begin(), data[row - 1].end(), item);
-
     try
     {
-		if(data[row-1].empty())	throw 42;
-		else if(srch == data[row-1].end()) throw 99;
-		else ++srch;
+		if(data[row - 1].empty())	throw 42;
+		else if(item >= data[row - 1].size())	throw 99;
+
 	}catch(int err){
 
 		switch(err)
 		{
-			case 42: //If selected container is empty.
-				std::cerr << "Error " << err << ", attempted to unlock secret of life. "
-                         		  << "Please reenter your menu selection.";
-				break;
-			case 99: //If selected data within container doesn't exist.
-				std::cerr << "Error " << err << " ocurred. Please enter an existing number from the list.";
-				break;
+			case 42:
+			    {
+			        std::cerr << "Error " << err << ", attempted to unlock secret of life. "
+                         << "Please reenter your menu selection.";
+
+                    std::string enumSelect; //Makeshift error handler until something better comes around.
+                    std::cin >> enumSelect; //No input function that supports strings.
+                    row = Storage::selectMenu(enumSelect);
+
+                    return getData(row, item);
+
+			    }
+			case 99:
+			    {
+			        std::cerr << "Error " << err << " ocurred. Please enter an existing number from the list.";
+
+			        std::cin >> item; //Makeshift error handler. Current input function requires creating
+			        		  //a new array. Input from that doesn't translate back to main().
+
+			        return getData(row, item);
+			    }
 			default:
 				std::cerr << "Unknown error " << err;
 		}
-		std::cout << std::endl;
+		 std::cout << std::endl;
 	}
 
-    return *(srch - 1);
+    return data[row - 1][item];
 }
