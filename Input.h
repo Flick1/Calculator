0417ec0ae2364dee5206ce300ae4ee1e2b100463
Olysold <rotmgbiz@gmail.com>
2013-04-27 04:32:42 +0800

    Added prototype for new input funct.

--- Input.h
+++ Input.h
@@ -5,6 +5,9 @@
 namespace input
 {
     void get(int operands, double* input);
+    size_t isNumerical_Selection(size_t&);
+    double isOperand(double&);
+    string isString_selection(string&);
 }
 
 #endif // _INPUT_H_
