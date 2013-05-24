af99b2c46da8a7ee15e36945df87f7ec2cc9bfbb
Olysold <rotmgbiz@gmail.com>
2013-05-25 04:38:27 +0800

    Support new menu.

--- Screen.h
+++ Screen.h
@@ -1,19 +1,21 @@
 #ifndef SCREEN_H_INCLUDED
 #define SCREEN_H_INCLUDED
 
-#include <curses.h>
-#include <string>
+#include<curses.h>
+#include<string>
+#include<vector>
 
 namespace Screen
 {
-    WINDOW *create_screen(int startY, int startX, int height, int width, bool flag, chtype num);
+    WINDOW *create_screen(WINDOW *win,  int height, int width, bool flag = FALSE, char* num = "a", bool hightlight = FALSE);
 };
 
 namespace String_Ops
 {
     double convert(std::string &s);
     char* convert_print(double res);
+    double parse(std::string &s);
+    std::vector<std::string> calculate(std::vector<std::string> &, const std::string);
 };
-
 
 #endif // SCREEN_H_INCLUDED
