05b3925cc44af7641c3f46026a2f66198c934519
Olysold <rotmgbiz@gmail.com>
2013-06-04 19:39:54 +0800

    Added special ops and expression validity support.

--- Screen.cpp
+++ Screen.cpp
@@ -52,8 +52,7 @@ char* String_Ops::convert_print(double res)
     return ret;
 }
 
-
-double String_Ops::parse(std::string& s)
+std::vector<std::string> String_Ops::tokenize (const std::string&s)
 {
     std::vector<std::string> tokenize;
     std::string token;
@@ -65,22 +64,35 @@ double String_Ops::parse(std::string& s)
         tokenize.push_back(token);
     }
 
-    while (tokenize.size() > 1)
+    return tokenize;
+}
+
+double String_Ops::parse(std::string& s)
+{
+    std::vector<std::string> token = tokenize(s);
+
+    while (token.size() > 1)
     {
-        String_Ops::parentheses(tokenize);
-        String_Ops::calculate(tokenize, "*");
-        String_Ops::calculate(tokenize, "/");
-        String_Ops::calculate(tokenize, "-");
-        String_Ops::calculate(tokenize, "+");
+        String_Ops::parentheses(token);
+        String_Ops::spec_ops(token);
+        String_Ops::unary_calculate(token, "cos");
+        String_Ops::unary_calculate(token, "sin");
+        String_Ops::unary_calculate(token, "tan");
+        String_Ops::unary_calculate(token, "log");
+        String_Ops::unary_calculate(token, "s.r");
+        String_Ops::binary_calculate(token, "*");
+        String_Ops::binary_calculate(token, "/");
+        String_Ops::binary_calculate(token, "-");
+        String_Ops::binary_calculate(token, "+");
     }
 
-    auto operand = tokenize.begin();
+    auto operand = token.begin();
     double ret = String_Ops::convert(*(operand));
 
     return ret;
 }
 
-void String_Ops::calculate(std::vector<std::string> &str_expression, const std::string str_operator)
+void String_Ops::binary_calculate(std::vector<std::string> &str_expression, const std::string str_operator)
 {
     for (auto iter = str_expression.begin(); iter != str_expression.end(); ++iter)
     {
@@ -115,6 +127,42 @@ void String_Ops::calculate(std::vector<std::string> &str_expression, const std::
     }
 }
 
+void String_Ops::unary_calculate(std::vector<std::string> &str_expression, const std::string str_operator)
+{
+    for (auto iter = str_expression.begin(); iter != str_expression.end(); ++iter)
+    {
+        if (*iter == str_operator && String_Ops::is_num(iter + 1))
+        {
+            double operand = String_Ops::convert(*(iter + 1));
+            double result = 0;
+
+            if (str_operator == "cos")
+                result = operations::Cosine(operand);
+            if (str_operator == "sin")
+                result = operations::Sine(operand);
+            if (str_operator == "tan")
+                result = operations::Tangent(operand);
+            if (str_operator == "log")
+                result = operations::Logarithm_Base10(operand);
+            if (str_operator == "s.r")
+                { /*Missing*/ }
+
+            std::string restore;
+
+            std::ostringstream oss;
+
+            oss << result;
+            restore = oss.str();
+
+            auto new_pos = str_expression.erase(iter, iter + 2);
+            str_expression.insert(new_pos, restore);
+
+            iter = str_expression.begin();
+        }
+    }
+}
+
+
 void String_Ops::parentheses(std::vector<std::string> &str_expression)
 {
     bool flag = false;
@@ -143,36 +191,41 @@ void String_Ops::parentheses(std::vector<std::string> &str_expression)
 
             do
             {
-                std::set<std::string> operatorSet { "*", "/", "+", "-" };
+                std::set<std::string> basic_ops { "*", "/", "+", "-" };
+                std::set<std::string> special_ops { "cos", "sin", "tan", "log", "s.r" };
 
-                String_Ops::calculate(paren_simp, "*");
-                String_Ops::calculate(paren_simp, "/");
-                String_Ops::calculate(paren_simp, "-");
-                String_Ops::calculate(paren_simp, "+");
+                String_Ops::unary_calculate(paren_simp, "cos");
+                String_Ops::unary_calculate(paren_simp, "tan");
+                String_Ops::unary_calculate(paren_simp, "sin");
+                String_Ops::unary_calculate(paren_simp, "log");
+                String_Ops::unary_calculate(paren_simp, "s.r");
+                String_Ops::binary_calculate(paren_simp, "*");
+                String_Ops::binary_calculate(paren_simp, "/");
+                String_Ops::binary_calculate(paren_simp, "-");
+                String_Ops::binary_calculate(paren_simp, "+");
 
                 //Processing final result of simplification
                 if (paren_simp.size() == 1)
                 {
                     //Checking open bracket
                     if (beginParen != str_expression.begin()) //Check that open bracket is NOT the initial element
-
-                        if (operatorSet.find(*(beginParen - 1)) == operatorSet.end() &&
-                            *(beginParen - 1) != "(" )                                  //Check element before open bracket is a number
+                    {
+                        if (basic_ops.find(*(beginParen - 1)) == basic_ops.end() &&
+                            *(beginParen - 1) != "(" &&
+                            special_ops.find(*(beginParen - 1)) == special_ops.end())  //Check element before open bracket is a number
                         {
                             str_expression.insert(beginParen, "*");
                         }
+                    }
 
                     //Checking close bracket
                     if (endParen + 1 != str_expression.end()) //Close bracket is NOT the last element.
                     {
-                        if (operatorSet.find(*(endParen + 1)) == operatorSet.end() &&
-                            *(endParen + 1) != ")" || *(endParen + 1) == "(" )        //Check element after close bracket is a number
+                        if (basic_ops.find(*(endParen + 1)) == basic_ops.end() &&
+                            *(endParen + 1) != ")" || *(endParen + 1) == "(" ||
+                            special_ops.find(*(endParen + 1)) != special_ops.end()) //Check element after close bracket if its a number, closed bracket, or spec ops.
                         {
                             endParen = str_expression.insert(endParen + 1, "*") - 1;
-                        }
-                        else
-                        {
-                            /*Note to self: Special operators (cos, sin, tan, "(", etc.) needs special consideration here.*/
                         }
                     }
 
@@ -188,10 +241,81 @@ void String_Ops::parentheses(std::vector<std::string> &str_expression)
     }
 }
 
-
-/*
-void chk_string(const std::string &str)
+void String_Ops::spec_ops(std::vector<std::string> &token)
 {
-    std::set<char> function_list { '+', '*', '-', '/' };
+    std::set<std::string> basic_ops { "*", "/", "+", "-" };
+    std::set<std::string> special_ops { "cos", "sin", "tan", "log", "s.r" };
+
+    for (auto iter = token.begin(); iter != token.end(); ++iter)
+    {
+        if (special_ops.find(*iter) != special_ops.end() &&
+            iter != token.begin())
+        {
+            //If left-hand side is a number, add *
+            if (iter != token.begin() && String_Ops::is_num(iter))
+            {
+                token.insert(iter, "*");
+                iter = token.begin();
+            }
+        }
+    }
 }
-*/
+
+
+bool String_Ops::chk_string(const std::string &s)
+{
+    std::vector<std::string> token = tokenize(s);
+
+    std::set<std::string> basic_ops { "+", "-", "*", "/" };
+    std::set<std::string> special_ops { "cos", "sin", "tan", "log", "s.r" };
+
+    if (basic_ops.find(*token.begin()) != basic_ops.end() ||
+        basic_ops.find(*(token.end() - 1)) != basic_ops.end() &&
+        special_ops.find(*(token.end() - 1)) != special_ops.end())
+        {
+            return false;
+        }
+
+    for (auto iter = token.begin(); iter != token.end(); ++iter)
+    {
+        //Determining validity of basic operations
+        if (basic_ops.find(*iter) != basic_ops.end())
+        {
+            //Left-hand side
+            if (basic_ops.find(*(iter - 1)) != basic_ops.end() ||
+                special_ops.find(*(iter - 1)) != special_ops.end()) { return false; }
+
+            //Right-hand side
+            if (basic_ops.find(*(iter + 1)) != basic_ops.end() ||
+                *iter == ")")                                       { return false; }
+        }
+
+        //Determining validity of parentheses
+        if (*iter == "(")
+        {
+            if (*(iter + 1) == ")" ||
+                basic_ops.find(*(iter + 1)) != basic_ops.end()) return false;
+
+            //Something about misc operations.
+        }
+
+        //All posibilities of special ops are valid
+    }
+    return true;
+}
+
+bool String_Ops::is_num(const std::vector<std::string>::iterator &iter)
+{
+    std::set<std::string> basic_ops { "+", "-", "*", "/" };
+    std::set<std::string> special_ops { "cos", "sin", "tan", "log", "s.r" };
+
+    if (basic_ops.find(*iter) == basic_ops.end() &&
+        special_ops.find(*iter) == special_ops.end() &&
+        *iter != "(" && *iter != ")")
+    {
+        return true;
+    }
+    else
+        return false;
+}
+
