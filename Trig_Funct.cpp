5e80fc9cd8219d34336fe28d4de4354ba56f4b40
Olysold <rotmgbiz@gmail.com>
2013-06-11 19:14:35 +0800

    Square root added

--- Trig_Funct.cpp
+++ Trig_Funct.cpp
@@ -7,3 +7,4 @@ double operations::Sine(double Number) {return (sin (Number*PI__/180));}
 double operations::Cosine(double Number) {return (cos (Number*PI__/180));}
 double operations::Tangent(double Number) {return (tan (Number*PI__/180));}
 double operations::Tangent2(double Opposite, double Adjacent) {return (tan ((Opposite/Adjacent)*PI__/180));}
+double operations::Square_Root(double Number) {return (sqrt(Number)); }
