2e5dc381bf4180c0a4eb9c221eeab2050af8c114
Daleth7 <i.publish.docs@gmail.com>
2013-06-11 20:50:04 -0600

    Commented

--- Trig_Funct.cpp
+++ Trig_Funct.cpp
@@ -8,3 +8,17 @@ double operations::Cosine(double Number) {return (cos (Number*PI__/180));}
 double operations::Tangent(double Number) {return (tan (Number*PI__/180));}
 double operations::Tangent2(double Opposite, double Adjacent) {return (tan ((Opposite/Adjacent)*PI__/180));}
 double operations::Square_Root(double Number) {return (sqrt(Number)); }
+/**
+ * Square root is not a trigonomic expression.
+ *    Trigonomic expressions deal with angles
+ *    such as the following trigonomic identities:
+ *    (sin(x))^2 + (cos(x))^2 = 1
+ *    cos(2x) = (cos(x))^2 - (sin(x))^2
+ *    where x is an angle.
+ *    Trigonomic expressions are usually associated
+ *    with sine, cosine, tangent, cotangent, etc.
+ * Also, Dreamer already wrote a square root
+ *    function in his Power & Square.cpp
+ *    file. Yes, I agree that his file's name
+ *    isn't so clear about its contents.
+**/
